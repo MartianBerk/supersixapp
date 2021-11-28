da1308a3571f568c5e19fa92048a7026ffa7f0dd
martianberk <baker.bsc@gmail.com>
2021-11-28 16:42:25 +0000

    tweaks

--- src/components/container/Games.js
+++ src/components/container/Games.js
@@ -241,7 +241,14 @@ class Games extends Component {
                     : null
                 }
                 {rows.length === 0 ? null : rows}
-                <div className="games-whitespace"></div>
+                <div className="games-whitespace">
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                </div>
             </div>
         )
     }
