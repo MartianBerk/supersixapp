cb18123b4e7a36e77a9a683bde6abcbd5188d2a9
MartianBerk <baker.bsc@gmail.com>
2022-02-06 10:26:54 +0000

    Fix 20220206 1 (#25)
    * Update requests.js
    
    * Update Head.js

--- src/components/requests.js
+++ src/components/requests.js
@@ -28,7 +28,7 @@ export class Requests {
         method = this.DEVELOPMENT ? "GET" : method || "GET";
         headers = headers || {};
         body = this.DEVELOPMENT ? null : body;
-        credentials = this.DEVELOPMENT ? "include" : credentials || "include";
+        credentials = this.DEVELOPMENT ? undefined : credentials || "include";
 
         let url = null;
         
@@ -59,7 +59,6 @@ export class Requests {
                 return resolve(res)
             })
             .catch(err => {
-                alert(err)
                 return reject(err)
             })
         ))   
