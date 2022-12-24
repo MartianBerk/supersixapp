9e2e68156b8be8ec772dd93af296f3b9519bb892
MartianBerk <baker.bsc@gmail.com>
2022-12-24 12:20:47 +0000

    Update requests.js

--- src/components/requests.js
+++ src/components/requests.js
@@ -2,7 +2,7 @@ import * as Constants from "./constants";
 
 
 export class Requests {
-    DEVELOPMENT = true;
+    DEVELOPMENT = false;
     OVERRIDES = {
         MATCHESURL: "matches.json",
         SCORESURL: "scores.json",
