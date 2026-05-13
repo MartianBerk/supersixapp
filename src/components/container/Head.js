a0d1b1ab536e89e400579188c252f05c660a2602
MartianBerk <baker.bsc@gmail.com>
2026-05-13 20:44:32 +0100

    Soccer cup (#74)
    * Update banner text for Qatar Hero and Euro Wizard
    
    * Change button width from 25% to 20%
    
    * Uncomment QatarHero menu functionality
    
    * Update Head.js

--- src/components/container/Head.js
+++ src/components/container/Head.js
@@ -175,10 +175,10 @@ class Head extends Component {
             bannerText = "Admin Mode";
         }
         else if (this.state.qatarHero) {
-            bannerText = "Qatar Hero"
+            bannerText = "Money Ball"
         }
         else if (this.state.euroWizard) {
-            bannerText = "Euro Wizard"
+            bannerText = "Euros"
         }
  
         return (
